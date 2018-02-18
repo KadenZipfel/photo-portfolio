2e349b6b3c17621991b70e464bbba409873346ae
Kaden Zipfel <h.fan98@hotmail.com>
2018-02-18 09:57:22 +0000

    First commit

---
+++ app.js
@@ -0,0 +1,16 @@
+$('.carousel').carousel({
+  interval: 3000
+})
+
+var elem = document.querySelector('.grid');
+var msnry = new Masonry( elem, {
+  // options
+  itemSelector: '.grid-item',
+  columnWidth: 200
+});
+
+// element argument can be a selector string
+//   for an individual element
+var msnry = new Masonry( '.grid', {
+  // options
+});
